9ef4cd652f5b882d2567a8bb5ed801c7a3e700ef
dannyhoover <dofjrtd@gmail.com>
2020-08-06 12:58:45 -0700

    Script updates and timer functionality

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+// timer
+var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 75;
+
+function setTime() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
+
+        // if (secondsLeft === 0) {
+        //     clearInterval(timerInterval);
+        // }
+
+    }, 1000);
+}
+
+// quiz question and answer section
 var startBtn = document.querySelector("#start-button");
+var questionDisplay = document.querySelector("#question-target");
+var answer1Display = document.querySelector("#answer-target1");
+var answer2Display = document.querySelector("#answer-target2");
+var answer3Display = document.querySelector("#answer-target3");
 
 var questions = [
     {
@@ -10,12 +33,39 @@ var questions = [
         title: "question 2: ",
         choices: ["choice1", "choice2", "choice3"],
         answer: "choice3"
+    },
+    {
+        title: "question 3: ",
+        choices: ["choice1", "choice2", "choice3"],
+        answer: "choice1"
+    },
+    {
+        title: "question 4: ",
+        choices: ["choice1", "choice2", "choice3"],
+        answer: "choice2"
     }
 ];
 
-function startQuiz() {
-    questionDisplay.textContent = questions[0].title;
-    console.log("HM 4 Starting Point")
+// high score function
+function score() {
+
 }
+
+function startQuiz() {
+    setTime();
+    for (var i=0; i < questions.length; i++) {
+        questionDisplay.textContent = questions[i].title;
+        answer1Display.textContent = questions[i].choices[0];
+        answer2Display.textContent = questions[i].choices[1];
+        answer3Display.textContent = questions[i].choices[2];
+
+        
+    }
+
+    // answer1Display.addEventListener("click", startQuiz)
+    // answer2Display.addEventListener("click", startQuiz)
+    // answer3Display.addEventListener("click", startQuiz)
+    
+};
 
 startBtn.addEventListener("click", startQuiz);
